f7a9c2532dc712cf51bff0dba1d0079958e0affc
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-04-05 14:30:28 +0400

    Update interface

--- ConfigManager.go
+++ ConfigManager.go
@@ -1,23 +1,23 @@
 package gonfig
 
 import (
-	golist "github.com/Aranyak-Ghosh/golist/implementations"
+	"github.com/Aranyak-Ghosh/golist"
 	"github.com/Aranyak-Ghosh/gonfig/types"
 )
 
 type configManager struct {
 	providers golist.List[types.Provider]
-	config    any
+	config    map[string]interface{}
 }
 
 type ConfigManager interface {
 	AddProvider(provider types.Provider)
-	ParseConfig(*any)
-	ReloadConfig(*any)
+	ParseConfig(any)
+	ReloadConfig(any)
 }
 
 var _ ConfigManager = (*configManager)(nil)
 
 func (cm *configManager) AddProvider(provider types.Provider) {}
-func (cm *configManager) ParseConfig(*any)                    {}
-func (cm *configManager) ReloadConfig(*any)                   {}
+func (cm *configManager) ParseConfig(any)                     {}
+func (cm *configManager) ReloadConfig(any)                    {}
