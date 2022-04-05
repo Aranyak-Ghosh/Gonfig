b931144068e2c416ca782048f42f39ab570ad407
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-04-05 20:47:55 +0400

    Added yaml provider

---
+++ internal/yaml/Provider.go
@@ -0,0 +1,42 @@
+package yaml
+
+import (
+	"io/ioutil"
+	"strings"
+
+	"github.com/Aranyak-Ghosh/gonfig/types"
+	"gopkg.in/yaml.v2"
+)
+
+type yamlProvider struct {
+	fileName string
+}
+
+var _ types.Provider = (*yamlProvider)(nil)
+
+const blank = " \t\r\n"
+
+func isNullOrEmpty(str string) bool {
+	return strings.Trim(str, blank) == ""
+}
+
+func (yp *yamlProvider) Load(res map[string]interface{}) error {
+	if !isNullOrEmpty(yp.fileName) {
+		if data, err := ioutil.ReadFile(yp.fileName); err != nil {
+			return err
+		} else {
+			err := yaml.Unmarshal(data, &res)
+			if err != nil {
+				return err
+			}
+		}
+
+	}
+	return nil
+}
+
+func NewYamlProvider(filename string) types.Provider {
+	return &yamlProvider{
+		fileName: filename,
+	}
+}
