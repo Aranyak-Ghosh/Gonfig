d20dd01794697a3b72a36089ae86f46a7bdb6584
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-04-06 23:13:20 +0400

    Added more tests

--- ConfigManager_test.go
+++ ConfigManager_test.go
@@ -1,6 +1,12 @@
 package gonfig
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Aranyak-Ghosh/golist"
+	"github.com/mitchellh/mapstructure"
+)
 
 func TestAddEnvProvider(t *testing.T) {
 	cm := NewConfigManager()
@@ -31,7 +37,13 @@ func TestAddEnvProvider(t *testing.T) {
 func TestAddJsonFileProvider(t *testing.T) {
 	cm := NewConfigManager()
 
-	var expected = "Bars"
+	type conf struct {
+		FooAgain []string
+	}
+
+	var got golist.List[string]
+
+	var expected = []string{"Bars"}
 
 	err := cm.AddProvider(Provider{
 		ProviderType: JSON,
@@ -42,14 +54,47 @@ func TestAddJsonFileProvider(t *testing.T) {
 		t.Errorf("Failed to add dotenv provider, %v", err)
 	}
 
-	dat, err := cm.GetConfig("Foo")
+	dat, err := cm.GetConfig("Foo__FooAgain")
 
 	if err != nil {
 		t.Errorf("Failed to get expected key %s from dotenv provider, %v", "Foo", err)
 	}
 
-	if dat.(string) != expected {
+	mapstructure.Decode(dat, &got)
+
+	if reflect.DeepEqual(got, expected) {
 		t.Errorf("Expected %s but got %v", expected, dat)
+	}
+}
+
+func TestMapJsonFileProvider(t *testing.T) {
+	cm := NewConfigManager()
+
+	type conf struct {
+		FooAgain []string
+	}
+
+	var got golist.List[string]
+
+	var expected = []string{"Bars"}
+
+	err := cm.AddProvider(Provider{
+		ProviderType: JSON,
+		FileName:     "appsettings.json",
+	})
+
+	if err != nil {
+		t.Errorf("Failed to add dotenv provider, %v", err)
+	}
+
+	err = cm.MapConfig("Foo__FooAgain", &got)
+
+	if err != nil {
+		t.Errorf("Failed to get expected key %s from dotenv provider, %v", "Foo", err)
+	}
+
+	if reflect.DeepEqual(got, expected) {
+		t.Errorf("Expected %s but got %v", expected, got)
 	}
 }
 
